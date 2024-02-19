a5dfac632e9955a3541e776498e77c1468ae17da
Ampomah-Mensah <gideonamp104@gmail.com>
2024-02-19 10:05:58 -1200

    my first user input task

---
+++ user_input.py
@@ -0,0 +1,6 @@
+#using the input function
+
+name = input('Enter your name : ')
+age = input ('How old are you : ')
+location = input('Enter your location : ')
+print('Hello , ' + name + ' you are ' + age + ' years old ' + ' and live in ' + location)
